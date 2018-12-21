d75815f678126766b9de81934a68ce0650b5c4e5
JaneLMF <long.meifeng@pivosgroup.com.cn>
2018-12-22 02:06:42 +0800

    * modify enableCmdQR is 2 to suit ubuntu.

--- demo.py
+++ demo.py
@@ -61,7 +61,7 @@ def timerfun(sched_timer):
 
 
 if __name__ == '__main__':
-    itchat.auto_login(hotReload=True, enableCmdQR=2)
+    itchat.auto_login(hotReload=True, enableCmdQR=True)
     timerfun('2018-12-21T17:05:00')
 
 
